d4d1aa50d4ec6f3704692b6b70ebbc07346c6945
Ahmed Atoui <a.atoui@altqniah.sa>
2021-10-08 15:18:28 +0100

    fix: convert to composition function

--- src/components/Nav/NavMenuItem.js
+++ src/components/Nav/NavMenuItem.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {NavLink} from "react-router-dom";
 
-class NavMenuItem extends Component {
-    render() {
-        const {text, link, linkProps} = this.props;
-        return (
-            <li className="nav-item me-4">
-                <NavLink className="nav-link" to={link} activeClassName='active' {...linkProps}>{text}</NavLink>
-            </li>
 
-        );
-    }
+function NavMenuItem(props) {
+    const {text, link, linkProps} = props;
+    return (
+        <li className="nav-item me-4">
+            <NavLink className="nav-link" to={link} activeClassName='active' {...linkProps}>{text}</NavLink>
+        </li>
+
+    );
 }
+
 
 NavMenuItem.propTypes = {
     text: PropTypes.string.isRequired,
